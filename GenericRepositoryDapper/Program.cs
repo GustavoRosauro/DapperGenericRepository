fcc1d7a35fba2c30d6096231920657693a516019
GustavoRosauro <gustavosrosauro@gmail.com>
2020-10-07 21:16:08 -0300

    'GenericDapperRepository'

---
+++ GenericRepositoryDapper/Program.cs
@@ -0,0 +1,52 @@
+﻿using GenericRepositoryDapper.Model;
+using GenericRepositoryDapper.Repository;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace GenericRepositoryDapper
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var pessoaRepository = new PessoaRepository("Pessoa");
+            Console.WriteLine("Salvando na tabela de usuários");
+            var guid = Guid.NewGuid();
+            await pessoaRepository.InsertAsync(new Pessoa()
+            {
+                Nome = "Gustavo",
+                Id = guid,
+                Idade = 27
+            });
+            await pessoaRepository.UpdateAsync(new Pessoa()
+            {
+                Nome = "Gustavo Rosauro",
+                Id = guid,
+                Idade = 28
+            });
+            List<Pessoa> pessoas = new List<Pessoa>();
+            for (int i = 0; i < 100000; i++)
+            {
+                var id = Guid.NewGuid();
+                pessoas.Add(new Pessoa()
+                {
+                    Id = id,
+                    Nome = $"Gustavo {i}",
+                    Idade = i
+                });
+            }
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Console.WriteLine($"Inserido {await pessoaRepository.SaveRangeAsync(pessoas)}");
+
+            stopwatch.Stop();
+
+            var elapsedTime = stopwatch.ElapsedMilliseconds;
+            Console.WriteLine($"tempo {elapsedTime} ms");
+            Console.Read();
+        }
+    }
+}
---
+++ GenericRepositoryDapper/Repository/PessoaRepository.cs
@@ -0,0 +1,12 @@
+﻿using GenericRepositoryDapper.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericRepositoryDapper.Repository
+{
+    public class PessoaRepository : GenericRepository<Pessoa>
+    {
+        public PessoaRepository(string tablename) : base(tablename){}
+    }
+}
